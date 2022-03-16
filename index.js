0bb5fe23e18543cca8f9e4f6f32c6c59ed009e30
Nicholas Anderson <anderson.nick@me.com>
2022-03-16 13:15:20 -0700

    initial commit. completed core of excercise

---
+++ index.js
@@ -0,0 +1,36 @@
+console.log(`Hello World`)
+
+const message = document.querySelector(`#message`)
+
+function addMovie(event){
+    event.preventDefault()
+    const inputField = document.querySelector('input')
+    let movie = document.createElement('li')
+    let movieTitle = document.createElement('span')
+    movieTitle.textContent = inputField.value
+    movieTitle.addEventListener(`click`, crossOffMovie)
+    movie.appendChild(movieTitle)
+    let deleteButton = document.createElement('button')
+    deleteButton.textContent = 'X'
+    deleteButton.addEventListener('click', deleteMovie)
+    movie.appendChild(deleteButton)
+    document.querySelector('ul').appendChild(movie)
+    inputField.value = ``
+}
+
+function deleteMovie(event){
+    event.target.parentNode.remove()
+    message.textContent = `Movie deleted!`
+}
+
+function crossOffMovie(event){
+    event.target.classList.toggle(`checked`)
+    if(event.target.classList.contains(`checked`)){
+        message.textContent = `Movie watched`
+    } else{
+        message.textContent = `Movie added back`
+    }
+}
+
+document.querySelector('form').addEventListener('submit', addMovie)
+
